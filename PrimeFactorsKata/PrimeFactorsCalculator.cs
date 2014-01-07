07699606aca9298f2ff6123ef23b95d8890b79c3
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-07 13:21:50 -0500

    Passed the test for 4.

--- PrimeFactorsKata/PrimeFactorsCalculator.cs
+++ PrimeFactorsKata/PrimeFactorsCalculator.cs
@@ -8,12 +8,22 @@ namespace PrimeFactorsKata
 {
     public class PrimeFactorsCalculator
     {
+        private List<Int32> factors;
         public List<Int32> GeneratePrimes(Int32 numberToFactor)
         {
-            if(numberToFactor == 1)
-                return new List<Int32>();
-            if(numberToFactor < 4)
+            factors = new List<Int32>();
+                
+            if(numberToFactor < 4 && numberToFactor > 1)
                 return new List<Int32>{ numberToFactor };
+
+            if (numberToFactor % 2 == 0)
+            {
+                numberToFactor /= 2;
+                factors.Add(2);
+                factors.Add(numberToFactor);
+            }
+            
+            return factors;
         }
     }
 }
